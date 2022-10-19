1079023cb2a38e2382109dbba4f2f369e5c66737
audreybichelmeir <bichelmeir@chapman.edu>
2022-10-19 16:25:22 -0700

    player controller & camera (in review?)
    • player controller can move around, go up a ramp, and steps
    • the camera follows the player -> need unity asset cinemachine (package manager)

--- Assets/C# Scripts/Gameplay/PlayerController.cs
+++ Assets/C# Scripts/Gameplay/PlayerController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //public float speed = .01f;
+    //void Update()
+    //{
+    //    float xDirection = Input.GetAxis("Horizontal");
+    //    float zDirection = Input.GetAxis("Vertical");
+
+    //    Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+    //    transform.position += moveDirection * speed; 
+    //}
+
+
+    public float speed;
+    public float rotationSpeed;
+
+    private CharacterController characterController;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+        characterController.SimpleMove(movementDirection * magnitude);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
+
+/*Reference:
+ * https://www.youtube.com/watch?v=t6e2MvEG0Tc
+ * https://www.youtube.com/watch?v=Z2KmfduirfU
+ */
