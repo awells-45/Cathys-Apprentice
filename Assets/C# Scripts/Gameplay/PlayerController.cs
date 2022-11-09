21b9a836f5fce52148867abce436a6258a2af79f
Andrew Wells <awells@chapman.edu>
2022-11-08 17:44:16 -0800

    Added Player Footsteps

--- Assets/C# Scripts/Gameplay/PlayerController.cs
+++ Assets/C# Scripts/Gameplay/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /*References:
@@ -11,6 +13,14 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+    
+    // footsteps variables
+    public float stepFrequency;
+    public AudioSource footstepsSource;
+    public AudioClip leftStep;
+    public AudioClip rightStep;
+    private uint _stepValue = 0;
+    private bool _isLeftStep = true;
 
     private CharacterController characterController;
 
@@ -19,11 +29,17 @@ public class PlayerController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
     }
 
-    void Update() // this should be edited to use the input controller
+    void FixedUpdate() // this should be edited to use the input controller
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        if (!isFloatZeroed(horizontalInput) || !isFloatZeroed(verticalInput)) // if there is direction input
+        {
+            IncrementFootstepSound();
+            Debug.Log(_stepValue);
+        }
+        
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
         movementDirection.Normalize();
@@ -33,6 +49,27 @@ public class PlayerController : MonoBehaviour
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    bool isFloatZeroed(float testFloat) // TODO - this might be better to put into some type of Math Utils class
+    {
+        return ((testFloat < 0.05) && (testFloat > -0.05));
+    }
+
+    void IncrementFootstepSound()
+    {
+        _stepValue++;
+        uint stepLimit = (uint)(1.0f / stepFrequency);
+        if (_stepValue > stepLimit)
+        {
+            _stepValue = 0;
+            if (_isLeftStep)
+                footstepsSource.clip = leftStep;
+            else
+                footstepsSource.clip = rightStep;
+            _isLeftStep = !_isLeftStep;
+            footstepsSource.Play(0);
         }
     }
 }
